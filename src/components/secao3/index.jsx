a5df3d9e7c144028e6e652135e2262c441c9b57a
ryankkzy <ryan37370308@gmail.com>
2024-10-17 16:38:19 -0300

    secao 3 finalizada.

--- src/components/secao3/index.jsx
+++ src/components/secao3/index.jsx
@@ -2,19 +2,21 @@ import './index.scss'
 
 export default function Secao3() {
     return (
-        <div className='skills'>
-
-            <div titulo>
-                <h1>Skills:</h1>
+        <div className='secao3'>
+            <div className='titulo'>
+                <h2>Skills:</h2>
             </div>
-
-            <div className='img1'>
-            <img src="./src/image/html_2.webp" alt="" />
+            <div>
+                <h1>Habilidades e conhecimento</h1>
             </div>
-            
+            <div className='skills'>
+                <img className='html' src="/html_2.webp" alt=""/>
+                <img className='css' src="/css-3.webp" alt="" />
+                <img className= 'js' src="/script-java.webp" alt="" />
+                <img className='node' src="/node-js_1.webp" alt="" />
+                <img className='react' src="/react.webp" alt="" />
+                <img className='figma' src="/figma.webp" alt="" />
+            </div>        
         </div>
-
-
     )
-
 }
