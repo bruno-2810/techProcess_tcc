9c6abd98935360e5b592952093329981bc83ce74
mhvidarasa07 <miguelhortencio07@gmail.com>
2024-10-17 00:36:51 -0300

    bateu voltou Brunão

---
+++ src/components/secao2/index.jsx
@@ -0,0 +1,23 @@
+import './index.scss'
+
+
+export default function Secao2() {
+
+    <div className='Secao2'>
+        <div className='serviços'>
+            <h2>Serviços:</h2>
+
+            <h1>Nossas experiencias e serviços:</h1>
+
+            <p>UX/UI Design</p>
+            <p className='12'>12 Projetos</p>
+
+            <p>Colaboração</p>
+            <p className='8'>8 Projetos</p>
+
+            <p>Websites</p>
+            <p className='28'>28 Projetos</p>
+
+        </div>
+    </div>
+}
--- src/pages/inicio/App.jsx
+++ src/pages/inicio/App.jsx
@@ -1,12 +1,14 @@
 import Cabecalho from '../../components/cabecalho';
 import Secao1 from '../../components/secao1';
+import Secao2 from '../../components/secao2';
 import './App.scss';
 
 function App() {
   return (
     <div className='pagina-inicio'>
       <Cabecalho />
-      <Secao1/>
+      <Secao1 />
+      <Secao2 />
     </div>
   )
 }
