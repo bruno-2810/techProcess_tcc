0f37527e534109e2091b361f15c16bb0187dea56
brunin <ra55498652895@acaonsfatima.org.br>
2024-11-01 08:06:29 -0300

    frei

--- src/components/cabecalho/index.jsx
+++ src/components/cabecalho/index.jsx
@@ -15,7 +15,6 @@ export default function Cabecalho() {
         <a href="https://www.instagram.com/techprocess_/profilecard"><img src="/images/instaroxo.png" alt="" /></a>
         <a href="https://github.com/bruno-2810/techProcess_tcc.git"><img src="/images/mdi_github.png" alt="" /></a>
         <a href="https://www.linkedin.com/in/tech-process-8b86a1335/"><img src="/images/mdi_linkedin.png" alt="" /></a>
-        <img src="/images/material-symbols_mail-rounded.png" alt="" className='email' />
       </div>
 
     </div>
--- src/components/secao6/index.jsx
+++ src/components/secao6/index.jsx
@@ -7,7 +7,6 @@ export default function Secao6() {
             <h1>Entre em <span>contato</span></h1>
             <p>Para mais informações</p>
             <div className='contatos'>
-                <img className = "email" src="/images/emailbranco.png" alt=" "width = "60px" height="60px" />
                 <a href="https://chat.whatsapp.com/JRPSYGaRnvm0jZcO9YVBfL"><img class = "zap" src="/images/whatsappbranco.png" alt=" "width = "60px" height="60px"/></a>
                 <a href="https://www.instagram.com/techprocess_/profilecard"><img className='ig' src="/images/instagrambranco.png" alt="" width = "60px" height="60px"/></a> 
                 <a href="https://www.linkedin.com/in/tech-process-8b86a1335/"><img className='linkedin' src="/images/linkedinbranco.png" alt="" width = "60px" height="60px" /></a>
