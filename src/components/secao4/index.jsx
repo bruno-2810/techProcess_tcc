6b72d5513016b524815aa85c27a6fc91b0d9c43e
ryankkzy <ryan37370308@.com>
2024-11-13 09:42:25 -0300

    ajustando

--- src/components/secao4/index.jsx
+++ src/components/secao4/index.jsx
@@ -4,9 +4,7 @@ export default function Secao4() {
     return(
         <div className='Secao4'>
             <div className='js'><h1>JavaScript</h1></div>
-            
             <div className='node'><h1>NodeJs</h1></div>
-
             <div className='react'><h1>ReactJs</h1></div>
         </div>
     )
